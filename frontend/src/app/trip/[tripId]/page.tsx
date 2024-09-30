e48155b38a9500a296ea8f4cb456d7707481a9ab
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-01 02:39:11 +0900

    feat-get params in [tripId]

--- frontend/src/app/trip/[tripId]/page.tsx
+++ frontend/src/app/trip/[tripId]/page.tsx
@@ -1,7 +1,16 @@
-export default function Page() {
+type Props = {
+  params: { tripId: string }; // TripID
+  searchParams: { key: string | undefined }; // Public Key in params
+};
+
+export default function Page({ params, searchParams }: Props) {
+  const key = typeof searchParams.key === "string" ? searchParams.key : "";
   return (
     <div>
       <h1>TRIP_ID</h1>
+      {params.tripId}
+      <br />
+      {key}
     </div>
   );
 }
