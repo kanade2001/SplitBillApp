b357cd9ad5d7cb504f24d81929b73eafa56c2be7
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-02 17:00:48 +0900

    feat-表のスタイルの調整

--- frontend/src/app/trip/[tripId]/_components/payment-list/index.tsx
+++ frontend/src/app/trip/[tripId]/_components/payment-list/index.tsx
@@ -2,13 +2,23 @@ export default function PaymentList({ items }: { items: string[] }) {
   return (
     <div>
       LIST
-      <ul>
+      {/* TODO 要素がない場合の処理*/}
+      <div className="mx-10 border bor-der-gray-100">
         {items.map((item, index) => (
-          <li key={index}>
-            {index}: {item}
-          </li>
+          <div
+            key={index}
+            className={`grid grid-cols-5 items-center text-center bg-${
+              index % 2 === 0 ? "gray-600" : "gray-500"
+            }  py-4 `}
+          >
+            <div>{item}</div>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
