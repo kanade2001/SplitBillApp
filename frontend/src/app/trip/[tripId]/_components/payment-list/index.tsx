40219045acdab0e68ae1041fd02ceeca482231c1
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-10 05:55:59 +0900

    Feat-Reducerの追加

--- frontend/src/app/trip/[tripId]/_components/payment-list/index.tsx
+++ frontend/src/app/trip/[tripId]/_components/payment-list/index.tsx
@@ -1,7 +1,52 @@
+"use client";
+
+import { Key, useReducer } from "react";
+
 import { Info } from "@/components/Alert/Alert";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
 
+interface ItemState {
+  id: Key;
+  title: string;
+  member: string;
+  member_id: bigint;
+  currency: string;
+  currency_id: string;
+  amount: bigint;
+  datetime: Date;
+}
+
+type ItemAction =
+  | { type: "ADD_ITEM"; payload: { item: ItemState } }
+  | { type: "EDIT_ITEM"; payload: { item: ItemState } }
+  | { type: "DELETE_ITEM"; payload: { id: Key } };
+
+const initialState: ItemState[] = [];
+
+const ItemReducer = (state: ItemState[], action: ItemAction) => {
+  switch (action.type) {
+    // 追加
+    case "ADD_ITEM":
+      console.log("ADD_ITEM");
+      return [...state, action.payload.item];
+    // 編集
+    case "EDIT_ITEM":
+      console.log("EDIT_ITEM");
+      return state.map((item) =>
+        item.id === action.payload.item.id
+          ? { ...item, ...action.payload.item }
+          : item,
+      );
+    // 削除
+    case "DELETE_ITEM":
+      console.log("DELETE_ITEM");
+      return state.filter((item) => item.id !== action.payload.id);
+  }
+};
+
 export default function PaymentList({ items }: { items: string[] }) {
+  const [state, dispatch] = useReducer(ItemReducer, initialState);
+
   return (
     <div className="">
       <table className="w-full table-fixed border-collapse border border-gray-400">
