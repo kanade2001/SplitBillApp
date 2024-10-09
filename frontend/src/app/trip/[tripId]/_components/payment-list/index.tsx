c7d869ee2f0434e5494f7bc9516590397a776f0a
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-10 07:22:58 +0900

    Feat-Addを押したときのプルダウンメニューの作成

--- frontend/src/app/trip/[tripId]/_components/payment-list/index.tsx
+++ frontend/src/app/trip/[tripId]/_components/payment-list/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Key, useReducer } from "react";
+import { Key, useReducer, useState } from "react";
 
 import { Info } from "@/components/Alert/Alert";
 import "/node_modules/flag-icons/css/flag-icons.min.css";
@@ -21,7 +21,7 @@ type ItemAction =
   | { type: "EDIT_ITEM"; payload: { item: ItemState } }
   | { type: "DELETE_ITEM"; payload: { id: Key } };
 
-const initialState: ItemState[] = [];
+const initialStateList: ItemState[] = [];
 
 const ItemReducer = (state: ItemState[], action: ItemAction) => {
   switch (action.type) {
@@ -45,37 +45,50 @@ const ItemReducer = (state: ItemState[], action: ItemAction) => {
 };
 
 export default function PaymentList({ items }: { items: string[] }) {
-  const [state, dispatch] = useReducer(ItemReducer, initialState);
+  const [state, dispatch] = useReducer(ItemReducer, initialStateList);
 
   return (
     <div className="">
-      <table className="w-full table-fixed border-collapse border border-gray-400">
+      <table className="w-full table-fixed border border-gray-400">
         <thead className="bg-blue-800 text-white">
           <tr>
-            <th className="border border-b-4 border-double border-gray-400 p-2">
+            <th className="w-10 border border-r-0 border-gray-400 p-2">
+              <svg
+                className="h-6 w-6 text-gray-800 text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <rect
+                  width="12"
+                  height="12"
+                  x="6"
+                  y="6"
+                  stroke="currentColor"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  rx="1"
+                />
+              </svg>
+            </th>
+            <th className="border border-l-0 border-double border-gray-400 p-2">
               Title
             </th>
-            <th className="border border-b-4 border-double border-gray-400 p-2">
-              Memeber
-            </th>
-            <th className="w-28 border border-b-4 border-double border-gray-400 p-2">
-              Currency
-            </th>
-            <th className="border border-b-4 border-double border-gray-400 p-2">
-              Amount
-            </th>
-            <th className="border border-b-4 border-double border-gray-400 p-2">
-              Date/Time
-            </th>
-            <th className="w-16 border border-b-4 border-double border-gray-400 p-2">
-              Edit
-            </th>
+            <th className="border border-gray-400 p-2">Memeber</th>
+            <th className="w-28 border border-gray-400 p-2">Currency</th>
+            <th className="border border-gray-400 p-2">Amount</th>
+            <th className="border border-gray-400 p-2">Date/Time</th>
+            <th className="w-16 border border-gray-400 p-2">Edit</th>
           </tr>
         </thead>
         <tbody>
           {items.map((item, index) => (
             <tr key={index}>
-              <td className="border border-gray-400 p-2">{item}</td>
+              <td className="border border-r-0 border-gray-400"></td>
+              <td className="border border-l-0 border-gray-400 p-2">{item}</td>
               <td className="border border-gray-400 p-2">Kanade</td>
               <td className="border border-gray-400 p-2 text-center">
                 <div className="flex justify-between">
@@ -110,32 +123,7 @@ export default function PaymentList({ items }: { items: string[] }) {
           ))}
         </tbody>
         <tfoot>
-          <tr>
-            <th colSpan={6} className="border border-gray-400 bg-gray-800">
-              <div className="flex w-full">
-                <div className="p-2">
-                  <svg
-                    className="h-6 w-6 text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M5 12h14m-7 7V5"
-                    />
-                  </svg>
-                </div>
-                <div className="flex-grow p-2 text-left">Add</div>
-              </div>
-            </th>
-          </tr>
+          <AddRow />
         </tfoot>
       </table>
 
@@ -147,3 +135,107 @@ export default function PaymentList({ items }: { items: string[] }) {
     </div>
   );
 }
+
+const AddRow = () => {
+  const [isVisibleAdd, setIsVisibleAdd] = useState<boolean>(false);
+  const handleIsVisibleAdd = () => {
+    setIsVisibleAdd(!isVisibleAdd);
+  };
+
+  return (
+    <>
+      <tr>
+        <th className="border border-r-0 border-gray-400 bg-gray-800">
+          <svg
+            className="h-6 w-full text-gray-800 text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h14m-7 7V5"
+            />
+          </svg>
+        </th>
+        <th
+          colSpan={6}
+          className="border border-l-0 border-gray-400 bg-gray-800 text-white"
+        >
+          <button
+            className="h-full w-full p-2 text-left"
+            onClick={() => handleIsVisibleAdd()}
+          >
+            Add
+          </button>
+        </th>
+      </tr>
+      {isVisibleAdd && (
+        <>
+          <tr className="bg-gray-400">
+            <th></th>
+            <th className="p-2">
+              <input
+                type="text"
+                id="member"
+                className="w-full rounded-md border border-gray-600 bg-white px-2 text-sm text-gray-900"
+              />
+            </th>
+            <th className="p-2">
+              <input
+                type="text"
+                id="member"
+                className="w-full rounded-md border border-gray-600 bg-white px-2 text-sm text-gray-900"
+              />
+            </th>
+            <th className="p-2">
+              <input
+                type="text"
+                id="currency"
+                className="w-full rounded-md border border-gray-600 bg-white px-2 text-sm text-gray-900"
+                placeholder="JPY"
+              />
+            </th>
+            <th className="p-2">
+              <input
+                type="number"
+                id="amount"
+                className="w-full rounded-md border border-gray-600 bg-white px-2 text-sm text-gray-900"
+                placeholder="0"
+              />
+            </th>
+            <th className="p-2">
+              <input
+                type="datetime-local"
+                id="datetime"
+                className="w-full rounded-md border border-gray-600 bg-white px-2 text-sm text-gray-900"
+                placeholder={new Date().toISOString()}
+              />
+            </th>
+            <th></th>
+          </tr>
+          <tr className="bg-gray-400">
+            <th></th>
+            <th colSpan={5} className="px-2 pb-2">
+              <div className="flex justify-end">
+                <button className="x-20 ms-2 rounded-md bg-gray-800 px-2 text-center text-sm text-white">
+                  Reset
+                </button>
+                <button className="x-20 ms-2 rounded-md bg-blue-800 px-2 text-center text-sm text-white">
+                  Complete
+                </button>
+              </div>
+            </th>
+            <th></th>
+          </tr>
+        </>
+      )}
+    </>
+  );
+};
