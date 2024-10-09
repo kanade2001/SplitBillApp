c943f8fc8848a335e1aae054aaa684c0ec0fb3a4
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-09 22:53:46 +0900

    Feat-データがない場合のメッセージ表示

--- frontend/src/app/trip/[tripId]/_components/payment-list/index.tsx
+++ frontend/src/app/trip/[tripId]/_components/payment-list/index.tsx
@@ -1,24 +1,31 @@
+import { Info } from "@/components/Alert/Alert";
+
 export default function PaymentList({ items }: { items: string[] }) {
   return (
-    <div>
-      LIST
-      {/* TODO 要素がない場合の処理*/}
-      <div className="bor-der-gray-100 mx-10 border">
-        {items.map((item, index) => (
-          <div
-            key={index}
-            className={`grid grid-cols-5 items-center text-center bg-${
-              index % 2 === 0 ? "gray-600" : "gray-500"
-            } py-4`}
-          >
-            <div>{item}</div>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-          </div>
-        ))}
-      </div>
+    <div className="">
+      {items.length === 0 ? (
+        /* 要素がない場合 */
+        <div className="">
+          <Info title="データがありません" message="データを追加してください" />
+        </div>
+      ) : (
+        <div className="bor-der-gray-100 border">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className={`grid grid-cols-5 items-center text-center bg-${
+                index % 2 === 0 ? "gray-600" : "gray-500"
+              } py-4`}
+            >
+              <div>{item}</div>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
