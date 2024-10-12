36e8fcf85c094469668af03529398bdca3c8ec57
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-12 23:30:42 +0900

    Add-DropDownMenu

---
+++ frontend/src/components/ui/dropdown/index.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+interface DropDownProps {
+  items: (string | JSX.Element)[];
+}
+
+const DropDwown: React.FC<DropDownProps> = ({ items }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="relative w-full">
+      <div className="flex w-full justify-between rounded-md border border-gray-600 px-2 text-sm text-gray-900">
+        <div>Content</div>
+        <button onClick={handleOpen}>
+          <AngleDown />
+        </button>
+      </div>
+      {isOpen && (
+        <div className="bg-white-100 absolute w-full rounded-md border border-gray-600 p-2">
+          <ul className="flex flex-col items-center">
+            {items.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const AngleDown = () => {
+  return (
+    <svg
+      className="h-4 w-4 text-gray-800 dark:text-white"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="m19 9-7 7-7-7"
+      />
+    </svg>
+  );
+};
+
+export default DropDwown;
