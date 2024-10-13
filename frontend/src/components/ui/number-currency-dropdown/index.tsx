b9738100e1c4e97693a60a8d44c387753e49a4aa
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-13 10:22:32 +0900

    Fix-変数の名前の変更(currencyidvalueとの混同を避けるため)

--- frontend/src/components/ui/number-currency-dropdown/index.tsx
+++ frontend/src/components/ui/number-currency-dropdown/index.tsx
@@ -1,14 +1,14 @@
 interface NumberCurrencyDropDownProps {
-  numberid?: string;
+  numberfieldid?: string;
   numbererror: boolean;
   numbervalue: number;
   numberonChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 
-  currencyid?: string;
+  currencyfieldid?: string;
   currencyerror: boolean;
-  currencyvalue: string;
+  currencyidvalue: string; // currency id
   currencyonChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-  currencies: { key: string; item: string }[];
+  currencies: { key: string; item: string }[]; // currency list
 }
 
 const NumberCurrencyDropDown: React.FC<NumberCurrencyDropDownProps> = (
@@ -18,7 +18,7 @@ const NumberCurrencyDropDown: React.FC<NumberCurrencyDropDownProps> = (
     <div className="flex">
       <input
         type="number"
-        id={props.numberid ? props.numberid : "number-input"}
+        id={props.numberfieldid ? props.numberfieldid : "number-input"}
         className={[
           "block h-10 w-full rounded-s-md border border-e-gray-600 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500",
           props.numbererror
@@ -29,14 +29,14 @@ const NumberCurrencyDropDown: React.FC<NumberCurrencyDropDownProps> = (
         onChange={props.numberonChange}
       />
       <select
-        id={props.currencyid ? props.currencyid : "currency-select"}
+        id={props.currencyfieldid ? props.currencyfieldid : "currency-select"}
         className={[
           "block h-10 w-full rounded-e-md border border-s-0 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500",
           props.currencyerror
             ? "border-red-600 bg-red-200"
             : "border-gray-600 bg-white",
         ].join(" ")}
-        value={props.currencyvalue}
+        value={props.currencyidvalue}
         onChange={props.currencyonChange}
       >
         {props.currencies.map((currency) => (
