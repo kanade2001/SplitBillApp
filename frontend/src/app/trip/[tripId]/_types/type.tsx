60a79696645667d9f9f89778d747d3c1e044f53c
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-14 11:07:27 +0900

    Delete-interface FormStateを削除(ItemStateに統一)

--- frontend/src/app/trip/[tripId]/_types/type.tsx
+++ frontend/src/app/trip/[tripId]/_types/type.tsx
@@ -36,15 +36,6 @@ export const ItemReducer = (state: ItemState[], action: ItemAction) => {
   }
 };
 
-export interface FormState {
-  title: string;
-  memberid: number;
-  currencyid: string;
-  // TODO セントとかの扱い
-  amount: number;
-  datetime: Date;
-}
-
 export interface ErrorState {
   titleError: boolean;
   memberError: boolean;
