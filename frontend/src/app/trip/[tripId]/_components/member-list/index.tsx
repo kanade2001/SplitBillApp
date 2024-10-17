c109680c9f7d904f26ae4b0630d1e5f40f02a265
kanade2001 <kanade2001.cpp@gmail.com>
2024-10-18 08:34:39 +0900

    Delete-古い形式のテーブルを削除

--- frontend/src/app/trip/[tripId]/_components/member-list/index.tsx
+++ frontend/src/app/trip/[tripId]/_components/member-list/index.tsx
@@ -1,6 +1,4 @@
 import EditableTable from "@/components/table/table";
-import TableHeader from "@/components/table/table-header";
-import TableAddFooter from "@/components/table/table-add-footer";
 
 import { TextInput } from "@/components/ui";
 
@@ -12,18 +10,6 @@ const MemberList: React.FC<MemberListProps> = ({ id }) => {
   return (
     <div>
       <p>MemberList {id} works!</p>
-      <table className="w-full table-fixed border border-gray-400">
-        <TableHeader
-          key="member-table"
-          items={[
-            { key: "name", label: "Name" },
-            { key: "email", label: "Email" },
-            { key: "role", label: "Role" },
-            { key: "action", label: "Action" },
-          ]}
-        />
-        <TableAddFooter key="member-table-footer" col={4} />
-      </table>
 
       <EditableTable
         key="member-table"
