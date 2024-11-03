20ff00c52a464daad515d8a09ffb988ca06cd395
kanade2001 <kanade2001.cpp@gmail.com>
2024-11-03 21:20:46 +0900

    Feat-Pageの構成を書いた

--- frontend/src/app/trip/page.tsx
+++ frontend/src/app/trip/page.tsx
@@ -2,6 +2,12 @@ export default function Page() {
   return (
     <div>
       <h1>TRIP</h1>
+      <h2>User = User</h2>
+      <h2>Items</h2>
+      <button>Filter</button>
+      <button>Sort</button>
+      <button>Search</button>
+      <p>Display Item in Card</p>
     </div>
   );
 }
