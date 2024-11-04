62de054c528197b160988ec6627e135feba85a6f
kanade2001 <kanade2001.cpp@gmail.com>
2024-11-05 02:05:59 +0900

    Feat-Detail周りの関数を定義

--- frontend/src/types/trip.ts
+++ frontend/src/types/trip.ts
@@ -7,29 +7,16 @@ export interface Trip {
   title: string;
   created_at: Date;
   updated_at: Date;
-  members: MemberState[];
   status: string;
   description: string;
 }
 
-type Action =
-  | { type: "FETCH"; payload: { trips: Trip[] } }
-  | { type: "CREATE"; payload: { trip: Trip } }
-  | { type: "UPDATE"; payload: { id: string; trip: Trip } }
-  | {
-      type: "SORT";
-      payload: { sortBy: keyof Trip; orderBy: "ascending" | "descending" };
-    };
-
-export const initialTrip: Trip = {
-  id: "",
-  title: "",
-  created_at: new Date(),
-  updated_at: new Date(),
-  members: [],
-  status: "",
-  description: "",
-};
+export interface TripDetail extends Trip {
+  members: MemberState[];
+  currencies: [];
+  loading: boolean;
+  error: string | null;
+}
 
 interface TripState {
   trips: Trip[];
@@ -37,9 +24,40 @@ interface TripState {
   error: string | null;
 }
 
+type Action =
+  | { type: "FETCH"; payload: { trips: Trip[] } }
+  | {
+      type: "SORT";
+      payload: { sortBy: keyof Trip; orderBy: "ascending" | "descending" };
+    };
+
+type DetailAction =
+  | { type: "FETCH_DETAIL"; payload: { trip: TripDetail } }
+  | {
+      type: "SET_FIELD";
+      payload: { field: "title" | "status" | "description"; value: string };
+    };
+
+export const initialTrip: Trip = {
+  id: "",
+  title: "",
+  created_at: new Date(),
+  updated_at: new Date(),
+  status: "",
+  description: "",
+};
+
 const initialTripState: TripState = {
   trips: [],
-  loading: true,
+  loading: false,
+  error: null,
+};
+
+const initialTripDetail: TripDetail = {
+  ...initialTrip,
+  members: [],
+  currencies: [],
+  loading: false,
   error: null,
 };
 
@@ -49,24 +67,6 @@ function tripReducer(state: TripState, action: Action): TripState {
       return {
         ...state,
         trips: action.payload.trips,
-        loading: false,
-        error: null,
-      };
-    case "CREATE":
-      return {
-        ...state,
-        trips: [...state.trips, action.payload.trip],
-        loading: false,
-        error: null,
-      };
-
-    case "UPDATE":
-      const updatedtrips = state.trips.map((trip) =>
-        trip.id === action.payload.trip.id ? action.payload.trip : trip,
-      );
-      return {
-        ...state,
-        trips: updatedtrips,
         loading: false,
         error: null,
       };
@@ -89,16 +89,41 @@ function tripReducer(state: TripState, action: Action): TripState {
   }
 }
 
+function tripDetailReducer(
+  state: TripDetail,
+  action: DetailAction,
+): TripDetail {
+  switch (action.type) {
+    case "FETCH_DETAIL":
+      return {
+        ...state,
+        ...action.payload.trip,
+        loading: false,
+        error: null,
+      };
+    case "SET_FIELD":
+      return {
+        ...state,
+        [action.payload.field]: action.payload.value,
+        loading: false,
+        error: null,
+      };
+    default:
+      throw new Error("Unhandled action type");
+  }
+}
+
 export function useTrip() {
   const [state, dispatch] = useReducer(tripReducer, initialTripState);
 
+  // トリップ一覧を取得
   useEffect(() => {
     const fetchTrips = async () => {
+      // TODO: ローディングの表示
       try {
         // DEBUG
         // const response = await fetch("http://localhost:3001/trips");
         // const data = await response.json();
-
         // デバッグ用の固定値
         const data: Trip[] = [
           {
@@ -106,7 +131,6 @@ export function useTrip() {
             title: "Trip to Tokyo",
             created_at: new Date(),
             updated_at: new Date(),
-            members: [],
             status: "planned",
             description: "A trip to Tokyo",
           },
@@ -115,7 +139,6 @@ export function useTrip() {
             title: "Trip to Kyoto",
             created_at: new Date(),
             updated_at: new Date(),
-            members: [],
             status: "completed",
             description: "A trip to Kyoto",
           },
@@ -128,7 +151,73 @@ export function useTrip() {
     fetchTrips();
   }, []);
 
-  const createTrip = async (newTrip: Omit<Trip, "id">) => {
+  // ソート
+  const sortTrips = (
+    sortBy: keyof Trip,
+    orderBy: "ascending" | "descending",
+  ) => {
+    dispatch({ type: "SORT", payload: { sortBy, orderBy } });
+  };
+
+  return {
+    trips: state.trips,
+    loading: state.loading,
+    error: state.error,
+    sortTrips,
+  };
+}
+
+export function useTripDetail(id: string) {
+  const [state, dispatch] = useReducer(tripDetailReducer, {
+    ...initialTrip,
+    members: [],
+    currencies: [],
+    loading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    const fetchTrip = async () => {
+      try {
+        if (id === "newtrip") {
+          dispatch({
+            type: "FETCH_DETAIL",
+            payload: { trip: initialTripDetail },
+          });
+        } else {
+          // DEBUG
+          // const response = await fetch(`http://localhost:3001/trips/${id}`);
+          // const data = await response.json();
+          // デバッグ用の固定値
+          const data: TripDetail = {
+            id: "1",
+            title: "Trip to Tokyo",
+            created_at: new Date(),
+            updated_at: new Date(),
+            status: "planned",
+            description: "A trip to Tokyo",
+            members: [],
+            currencies: [],
+            loading: false,
+            error: null,
+          };
+          dispatch({ type: "FETCH_DETAIL", payload: { trip: data } });
+        }
+      } catch (error) {
+        throw new Error(`Error at fetchTrip: ${error}`);
+      }
+    };
+    fetchTrip();
+  }, [id]);
+
+  const setTripField = (
+    field: "title" | "status" | "description",
+    value: string,
+  ) => {
+    dispatch({ type: "SET_FIELD", payload: { field, value } });
+  };
+
+  const createTrip = async () => {
     try {
       // サーバー通信を行って新しいIDを取得
       /* TODO_SERVER
@@ -142,26 +231,15 @@ export function useTrip() {
       const createdTrip = await response.json();
       dispatch({ type: "CREATE", payload: { state: createdTrip } });
       */
-
       // DEBUG
-      dispatch({
-        type: "CREATE",
-        payload: {
-          trip: {
-            id: `${state.trips.length + 1}`,
-            ...newTrip,
-            created_at: new Date(),
-            updated_at: new Date(),
-          },
-        },
-      });
+      console.log("Create");
     } catch (error) {
       console.error(`Error at createTrip: ${error}`);
       throw new Error(`Error at createTrip: ${error}`);
     }
   };
 
-  const updateTrip = async (id: string, updatedTrip: Trip) => {
+  const updateTrip = async () => {
     try {
       // サーバー通信を行って既存のIDを使用して更新
       /* TODO_SERVER
@@ -173,26 +251,18 @@ export function useTrip() {
         body: JSON.stringify(updatedTrip),
       });
       */
-      dispatch({ type: "UPDATE", payload: { id, trip: updatedTrip } });
+      // DEBUG
+      console.log("Update");
     } catch (error) {
       console.error(`Error at updateTrip: ${error}`);
       throw new Error(`Error at updateTrip: ${error}`);
     }
   };
 
-  const sortTrips = (
-    sortBy: keyof Trip,
-    orderBy: "ascending" | "descending",
-  ) => {
-    dispatch({ type: "SORT", payload: { sortBy, orderBy } });
-  };
-
   return {
-    trips: state.trips,
-    loading: state.loading,
-    error: state.error,
+    state,
+    setTripField,
     createTrip,
     updateTrip,
-    sortTrips,
   };
 }
