088cc5c059fce1db489405c63ace1fa0ec53c5d6
kanade2001 <kanade2001.cpp@gmail.com>
2024-11-18 22:26:06 +0900

    Feat-Add Children Page in [tripid]

---
+++ frontend/src/app/trip/[tripId]/payment/page.tsx
@@ -0,0 +1,7 @@
+export default function TripPaymentPage() {
+  return (
+    <div>
+      <h1>Payment Page</h1>
+    </div>
+  );
+}
---
+++ frontend/src/app/trip/[tripId]/settings/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+import MemberTable from "@/components/table/_index/member-table";
+import { useMemberList } from "@/store/hooks/useMemberList";
+import { testMember1 } from "@/test/members";
+
+type Props = {
+  params: { tripId: string }; // TripID
+  searchParams: { key: string | undefined; tab: string | undefined }; // Public Key in params
+};
+
+export default function TripSettingsPage({ params, searchParams }: Props) {
+  const key = typeof searchParams.key === "string" ? searchParams.key : "";
+  const {
+    members,
+    addMember,
+    editMember,
+    deleteMember,
+    sortMembers,
+    filterMembers,
+  } = useMemberList({
+    initialMembers: testMember1,
+  });
+
+  return (
+    <div className="space-y-5 p-5">
+      <h1>TRIP_ID = {params.tripId}</h1>
+      <h2>PUBLIC_KEY = {key}</h2>
+      <h2>TAB = {searchParams.tab}</h2>
+
+      <p>NEW TABLE COMPONENT</p>
+      <MemberTable
+        members={members}
+        addMember={addMember}
+        editMember={editMember}
+        deleteMember={deleteMember}
+        sortMembers={sortMembers}
+        filterMembers={filterMembers}
+      />
+    </div>
+  );
+}
