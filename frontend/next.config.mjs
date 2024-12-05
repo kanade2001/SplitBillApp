42dbd8a9d1db03ce9340ddc58b90f9566dc2a5c3
kanade2001 <kanade2001.cpp@gmail.com>
2024-12-05 23:47:44 +0900

    ホットリロードの有効化

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000, // 1秒ごとに変更をチェック
+      aggregateTimeout: 300, // 変更があった場合、300ms待ってから再ビルド
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
