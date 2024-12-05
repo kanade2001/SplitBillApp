de751c6e2c4a3cad99cfd5068014955248858c3b
kanade2001 <kanade2001.cpp@gmail.com>
2024-12-05 23:50:40 +0900

    Fix: Error Message

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpackDevMiddleware: (config) => {
-    config.watchOptions = {
-      poll: 1000, // 1秒ごとに変更をチェック
-      aggregateTimeout: 300, // 変更があった場合、300ms待ってから再ビルド
-    };
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000, // 1秒ごとに変更をチェック
+        aggregateTimeout: 300, // 変更があった場合、300ms待ってから再ビルド
+      };
+    }
     return config;
   },
 };
