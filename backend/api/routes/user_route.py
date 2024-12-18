d2d0c198164b2448bdcff0381acb49947af09119
kanade2001 <kanade2001.cpp@gmail.com>
2024-12-18 11:51:35 +0000

    style: format

--- backend/api/routes/user_route.py
+++ backend/api/routes/user_route.py
@@ -4,9 +4,15 @@ from rest_framework.routers import DefaultRouter
 from api.views import UserViewSet
 
 router = DefaultRouter()
-router.register("", UserViewSet, basename="user")
+router.register("", UserViewSet, basename="user")  # basenameを指定
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("<str:username>/",UserViewSet.as_view({'get': 'retrieve', 'patch': 'update', 'delete': 'destroy'}) ,name="user-detail"),
+    path(
+        "<str:username>/",
+        UserViewSet.as_view(
+            {"get": "retrieve", "patch": "update", "delete": "destroy"}
+        ),
+        name="user-detail",
+    ),
 ]
--- backend/api/views/user_view.py
+++ backend/api/views/user_view.py
@@ -1,32 +1,32 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from api.models import User
 from api.serializers import UserSerializer
 from api.services import UserService
 
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all()  # 全てのユーザーを取得
     serializer_class = UserSerializer
-    lookup_field = "username"
+    lookup_field = "username"  # usernameをlookup_fieldに指定
 
     def perform_create(self, serializer):
-        UserService.create_user(
+        UserService.create_user(  # create_userメソッドで新規作成
             email=serializer.validated_data["email"],
             username=serializer.validated_data["username"],
             password=serializer.validated_data["password"],
         )
 
     def perform_update(self, serializer):
-        user = get_object_or_404(User, username=self.kwargs["username"])
-        UserService.update_user(
+        user = get_object_or_404(
+            User, username=self.kwargs["username"]
+        )  # usernameを取得
+        UserService.update_user(  # update_userメソッドで更新
             user,
             email=serializer.validated_data.get("email"),
             username=serializer.validated_data.get("username"),
             password=serializer.validated_data.get("password"),
         )
         serializer.save()
-    
